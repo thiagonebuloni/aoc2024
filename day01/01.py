8e84f33ba36625f5e3343e0abd27c2c0b3f5c87c
Thiago Nebuloni <thiago.nebuloni@gmail.com>
2024-12-02 21:13:23 -0300

    :recycle: cleaning up

--- day01/01.py
+++ day01/01.py
@@ -1,28 +1,10 @@
 INPUT: str = "day01/input.txt"
 EXAMPLE_INPUT: str = "day01/example_input.txt"
 
-
-def solve(array1: str, array2: str):
-    distances: list = []
-
-    array_sorted1 = sorted(list(array1))
-    array_sorted2 = sorted(list(array2))
-    print(array_sorted1)
-    print(array_sorted2)
-
-    for num1, num2 in zip(array_sorted1, array_sorted2):
-        distances.append(abs(int(num1) - int(num2)))
-        print(abs(int(num1) - int(num2)))
-
-    print()
-    print(sum(distances))
-    # input()
-    return sum(distances)
-
-
 distances: list = []
 array1: list = []
 array2: list = []
+
 with open(INPUT, "r") as file:
     for line in file:
         a1, a2 = line.split()
